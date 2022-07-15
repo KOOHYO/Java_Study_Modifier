a3167eb3175a8e312fa86f8dace4592b5e5e7383
KOOHYO <zpfhzpfh112@naver.com>
2022-07-15 16:06:37 +0900

    다형성

--- src/com/iu/main/Main.java
+++ src/com/iu/main/Main.java
@@ -1,14 +1,31 @@
 package com.iu.main;
 
 import com.iu.terran.Marine;
+import com.iu.terran.Scv;
+import com.iu.unit.Unit;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Marine m1 = new Marine();
-		m1.setHp(30);
-		m1.info();
-		m1.move();
+		Scv s1 = new Scv();
+		
+		//Marine is a Unit
+		//Scv    is a Unit
+		
+		Unit unit = m1;//자식클래스에서 부모클래스에 대입
+		
+		
+		
+		
+		System.out.println(unit.getHp()); //Unit이 가지고있는건 name까지이다. Marine에 있는 weapon이 있다
+		System.out.println(unit.getColor());
+		System.out.println(unit.getName());
+		
+		m1 = (Marine)unit;//여기에 Scv를 넣으면 에러 발생 부모클래스가 자식클래스로 대입은 안됀다 여기서 다형성
+		
+		System.out.println(m1.getWeapon());
+		
 	}
-
 }
